d8f1cd55a9abf89e76cee7b64746e14575c166d3
Moroccan-Ghost <aiman.elbouayadi@gmail.com>
2023-01-06 19:54:58 +0100

    update releveWS

--- src/main/mappingObject/soapPart/releveWS.java
+++ src/main/mappingObject/soapPart/releveWS.java
@@ -5,6 +5,8 @@ import jakarta.jws.WebService;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
+import operationServices.Operation;
+import operationServices.Operations;
 import operationServices.ReleveService;
 
 import java.io.File;
@@ -22,4 +24,38 @@ public class releveWS {
         return this.releve.getRib();
 
     }
+
+    @WebMethod
+    public double getSolde(){
+        return this.releve.getSolde();
+    }
+
+    @WebMethod
+    public Operations getOperations(){
+        return this.releve.getOperations();
+    }
+
+    @WebMethod
+    /*TODO: CHECK THIS METHOD*/
+    public Operations getCreditOperations(){
+        Operations ops = new Operations();
+        for (Operation operation : this.getOperations().getOperations()){
+            if(operation.getType()=="CREDIT"){
+                ops.AddOperations(operation);
+            }
+        }
+        return ops;
+    }
+    @WebMethod
+    /*TODO: CHECK THIS METHOD*/
+    public Operations getDebitOperations(){
+        Operations ops = new Operations();
+        for (Operation operation : this.getOperations().getOperations()){
+            if(operation.getType()=="DEBIT"){
+                ops.AddOperations(operation);
+            }
+        }
+        return ops;
+    }
+
 }
