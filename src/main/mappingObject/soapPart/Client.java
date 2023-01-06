d91040ac109dcb769f3c67390a8b6c0e291d57be
Moroccan-Ghost <aiman.elbouayadi@gmail.com>
2023-01-06 19:57:14 +0100

    Add simple client

---
+++ src/main/mappingObject/soapPart/Client.java
@@ -0,0 +1,10 @@
+package soapPart;
+
+import jakarta.xml.bind.JAXBException;
+
+public class Client {
+    public static void main(String[] args) throws JAXBException {
+        releveWS releve = new releveWS();
+        System.out.println(releve.getRib());
+    }
+}
